968424ba510a19d3925bedc665465d7995884f0e
David Anaeme <davidcanaeme@gmail.com>
2024-08-14 12:07:17 -0600

    Remove commented out answers

--- server.js
+++ server.js
@@ -1,47 +1,47 @@
 // // Use `.map()` to iterate over the following array:
 
-// const nums = [13, 87, 2, 89, 12, 4, 90, 63];
+const nums = [13, 87, 2, 89, 12, 4, 90, 63];
 
 // // Create a new array where each value is multiplied by 2 and log the new array.
 
 // // Your code here
-// const numsTimesTwo = nums.map((num) => {
-//     return num * 2;
-// });
+const numsTimesTwo = nums.map((num) => {
+    return num * 2;
+});
 
-// console.log(numsTimesTwo);
+console.log(numsTimesTwo);
 
 // // Given the following array, use destructuring to pull out the first and second values and place them into variables. Log both variables.
 
 // const pizzaToppings = ['Pineapple', 'Olives', 'Anchovies'];
 
 // // Your code here
-// const [toppingOne, toppingTwo] = pizzaToppings;
+const [toppingOne, toppingTwo] = pizzaToppings;
 
-// console.log(toppingTwo);
+console.log(toppingTwo);
 
 // // Given the following object, use destructuring to create variables `make` and `model` that will hold the respective values.
 
-// const car = {
-//     make: 'Audi',
-//     model: 'q5',
-//   };
+const car = {
+    make: 'Audi',
+    model: 'q5',
+  };
   
-// //   // Your code here
+// Your code here
 
-// const { make, model } = car;
+const { make, model } = car;
 
 // console.log(model);
 
 // // Duplicate the following array using the spread operator and assign it to `controversialPizzaToppings`. Then, log the variable.
 
-// const pizzaToppings = ['Pineapple', 'Olives', 'Anchovies'];
+const pizzaToppings = ['Pineapple', 'Olives', 'Anchovies'];
 
 // // Your code here
 
-// const controversialPizzaToppings = [...pizzaToppings];
+const controversialPizzaToppings = [...pizzaToppings];
 
-// console.log(controversialPizzaToppings);
+console.log(controversialPizzaToppings);
 
 // // Duplicate the following object and spread its values into a new variable `myCar`.
 
@@ -53,12 +53,12 @@
 // //   Change the `model` property of `myCar` to 'q7'. Log both objects.
   
 // //   Your code here
-// const myCar = {...car};
+const myCar = {...car};
 
-// myCar.model = 'q7';
+myCar.model = 'q7';
 
-// console.log('First:', car);
-// console.log('Clone:', myCar);
+console.log('First:', car);
+console.log('Clone:', myCar);
 
 
 // Create an object named userProfile. 
